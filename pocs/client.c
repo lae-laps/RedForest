a3d25efdd8e1468bf6ec03956fd17c59ea8604d7
lae-laps <unenlagiaa@gmail.com>
2022-08-11 19:20:14 +0200

    automatic commit > cli > at ./push.sh

--- pocs/client.c
+++ pocs/client.c
@@ -34,7 +34,6 @@ int main() {
     int sockfd, connfd;
     struct sockaddr_in servaddr, cli;
    
-    // socket create and verification
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd == -1) {
         printf("socket creation failed...\n");
@@ -44,12 +43,10 @@ int main() {
         printf("Socket successfully created..\n");
     bzero(&servaddr, sizeof(servaddr));
    
-    // assign IP, PORT
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = inet_addr(ADDRESS);
     servaddr.sin_port = htons(PORT);
    
-    // connect the client socket to server socket
     if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) {
         printf("connection with the server failed...\n");
         exit(0);
@@ -57,10 +54,8 @@ int main() {
     else
         printf("connected to the server..\n");
    
-    // function for chat
     func(sockfd);
-   
-    // close the socket
+
     close(sockfd);
 }
 
