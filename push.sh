76172cab0f5bd5d9a877cad45a31a22d37daff62
lae-laps <unenlagiaa@gmail.com>
2022-08-26 00:00:12 +0200

    changed push to be user-secure

--- push.sh
+++ push.sh
@@ -1,10 +1,34 @@
 #!/usr/bin/bash
+
+# git script to automatically commit the repo with default config
+# made by laelaps -> github.com/lae-laps/RedForest
+
+if [ $EUID != 0 ]
+then
+    echo "Starting GIT automatic CLI commit"
+    printf "=================================\n\n"
+else
+    echo "Avoid running GIT as root"
+    printf "=========================\n\n"
+fi
+
+alias push="./push"
+
 cd src 
+
 make clean
+
 cd ..
+
 git add --all
+
 git status
-#git commit -m "automatic commit > cli > at ./push.sh"
-git commit -m "$1"
+
+#git commit -m "automatic commit > cli > at ./push.sh" # <- auto commit
+
+git commit -m "$1" # use 1rst argument as commit message
+
 git status
+
 git push origin HEAD:master
+
